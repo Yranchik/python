b2017c16cb4eb138851eb1cc04e569dc9da46dd2
Yranchik <33001766+Yranchik@users.noreply.github.com>
2019-01-29 14:17:31 +0300

    Update test1.py
    Написан первый тест на питоне с использованнием Selenium WebDriver

--- test1.py
+++ test1.py
@@ -1,4 +1,12 @@
-from selenium.webdriver.firefox.webdriver import WebDriver
-import selenium.webdriver.commmon.action_chains
-import time, unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Firefox()
+driver.get("http://www.python.org")
+assert "Python" in driver.title
+elem = driver.find_element_by_name("q")
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
+assert "No results found." not in driver.page_source
+driver.close()
 
