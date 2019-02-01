83a0272f31453270a461820a43d4167a5204fbf2
Yranchik <33001766+Yranchik@users.noreply.github.com>
2019-02-01 17:45:54 +0300

    Create python4.py

---
+++ python4.py
@@ -0,0 +1,27 @@
+import unittest
+import re
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class PythonOrgSearch(unittest.TestCase):
+
+    def setUp(self):
+       self.driver = webdriver.Firefox()
+
+
+    def test_search_in_python_org(self):
+       driver = self.driver
+       driver.get("https://www.cbr.ru/")
+       driver.implicitly_wait(10)
+
+       course = driver.find_element_by_xpath("//td[@class='weak']")
+       x = int(course.text)
+       print(x)
+
+
+
+
